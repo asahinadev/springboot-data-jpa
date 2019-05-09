f3ed289ec9a1f93c71ee88d7f35ce9cff933c2d3
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-09 15:53:11 +0900

    ログアウト調整

--- src/main/java/com/example/spring/config/SecurityConfig.java
+++ src/main/java/com/example/spring/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.spring.db.service.UsersService;
 
@@ -69,7 +70,7 @@ public class SecurityConfig
 				.httpBasic().disable()
 
 				.logout()
-				.logoutUrl("/logout")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 				.logoutSuccessUrl("/login?logout")
 				.and()
 
